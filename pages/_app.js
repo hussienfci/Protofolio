5ad76e21a9e779fb5fef6cc765a724b2df0a29b6
hussienfci <hussienfci@gmail.com>
2025-05-07 23:26:27 +0200

    portofolio files

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+
+import '../styles/global.css'
+import {NavbarDemo} from '../_components/normal/Navbar'
+import { BackgroundBoxesDemo } from '../_components/normal/BackgroundBoxesDemo'
+
+ 
+export default function MyApp() {
+    return (
+        <div className='bg-black'>
+            
+      <NavbarDemo/> 
+      <BackgroundBoxesDemo/>
+
+        </div>
+    
+
+)}
